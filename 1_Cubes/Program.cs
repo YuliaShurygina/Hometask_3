57fd0c3704b59914cc97d4f339b321a27da591b2
Yulia Shurygina <zavalishina-yu@mail.ru>
2021-12-28 14:05:45 +0300

    Cubes from 1 to N

---
+++ 1_Cubes/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти кубы чисел от 1 до N
+Console.Write("Введите N = ");
+int N = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine();
+int i = 1;
+while (i<=N)
+{
+    int cube = i * i * i;
+    Console.WriteLine( "Куб числа " + i + " = " + cube);
+    i++;
+}
+
