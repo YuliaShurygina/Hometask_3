dc6fc11e55c6a2d288aedcf807e08f8f88115ad9
Yulia Shurygina <zavalishina-yu@mail.ru>
2021-12-28 14:31:41 +0300

    Multiplication of numbers from 1 to N

---
+++ 3_multiplication/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.Write("Введите N = ");
+int N = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine();
+int i = 1;
+int Multiplication = 1;
+while (i <= N)
+{
+    Multiplication = Multiplication * i;
+    i++;
+}
+Console.WriteLine("Произведение чисел от 1 до " + N + " равно " + Multiplication);
