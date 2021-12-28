dc9537b705e60dc4556accb3914a25cc94e28329
Yulia Shurygina <zavalishina-yu@mail.ru>
2021-12-28 14:18:20 +0300

    Sum of number's digits

---
+++ 2SumOfDigits/Program.cs
@@ -0,0 +1,15 @@
+﻿// Подсчитать сумму цифр в числе
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine()??"0");
+
+int count = 0;
+int sum = 0;
+
+while (A >= 1)
+{
+     int unity = A % 10;
+     A =  A / 10;
+     sum = sum + unity;
+     count++;
+}
+Console.WriteLine("Сумма цифр в числе = " + sum);
