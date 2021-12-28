6636a343c9a282e36bccb23a5c38e682ea4c1015
Yulia Shurygina <zavalishina-yu@mail.ru>
2021-12-28 14:55:37 +0300

    Cubes of even numbers  from 0 to N

---
+++ 4_Cubes_even/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.Write("Введите N = ");
+int N = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine();
+int i = 0;
+Console.WriteLine( "Кубы чисел, заканчивающихся на четную цифру: ");
+Console.WriteLine();
+while (i <= N)
+{
+    if (i % 2 ==0)
+    {
+        int cube = i * i * i;
+        Console.WriteLine( "Куб числа " + i + " = " + cube);
+    }
+    
+    i++;
+}
